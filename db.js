271dd78505c35f2704518e84ac4dedf912e10d96
Julio Oliveira <juliooliveira462@gmail.com>
2023-11-30 13:45:43 -0300

    Alteraçoes no db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://juliooliveira462:dYRzUZR0ORERTyaR@cluster0.zwcggqb.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://juliooliveira462:dYRzUZR0ORERTyaR@cluster0.zwcggqb.mongodb.net/login-escribo.Usuario?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });

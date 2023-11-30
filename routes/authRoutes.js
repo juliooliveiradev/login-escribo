58481efae8c9f36f7d1f116728c9d51f7deb71cf
Julio Oliveira <juliooliveira462@gmail.com>
2023-11-30 12:39:21 -0300

    Alteraçoes no authRouts

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/authMiddleware');
-const { signUp, signIn, getuser } = require('../controllers/authController');
+const { signUp, signIn, getUser } = require('../controllers/authController');
 
 
 router.post('/signup', signUp);
